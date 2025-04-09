73620f5c3c84d37877ffb62a373ed3505f939921
Devah-Tech <devahtech@gmail.com>
2025-04-09 15:28:14 -0400

    Adding the production version of notes-frontend

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
